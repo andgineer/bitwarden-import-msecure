3c2f07088c286a1301fedf695bbb582a9f34c4be
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-03-28 15:19:45 +0100

    refactored for multiple output formats

--- src/bitwarden_import_msecure/main.py
+++ src/bitwarden_import_msecure/main.py
@@ -1,13 +1,14 @@
 """Bitwarden Import mSecure Export."""
 
-import os
 import csv
 from pathlib import Path
-from typing import List, Dict, Tuple
 
 import rich_click as click
 
-OUTPUT_FILE_DEFAULT = "bitwarden.csv"
+from bitwarden_import_msecure.bitwarden_csv import BitwardenCsv
+from bitwarden_import_msecure.msecure import import_msecure_row
+
+OUTPUT_FILE_DEFAULT = "bitwarden"
 NOTES_MODE = "notes"
 
 
@@ -24,8 +25,15 @@ NOTES_MODE = "notes"
         f"By default, they are added as custom fields. Use '{NOTES_MODE}' to add them to notes."
     ),
 )
+@click.option(
+    "--format",
+    "output_format",
+    type=click.Choice(["csv", "json"]),
+    default="csv",
+    help="Output file format.",
+)
 def bitwarden_import_msecure(
-    input_file: str, output_file: str, force: bool, extra_fields: str
+    input_file: str, output_file: str, force: bool, extra_fields: str, output_format: str
 ) -> None:
     """
     Converts file `INPUT_FILE` exported from mSecure to Bitwarden compatible format
@@ -33,111 +41,31 @@ def bitwarden_import_msecure(
 
     1.Export CSV from mSecure
     2.Run this script on the exported CSV file
-    3.Import the processed file into Bitwarden a Bitwarden CSV
+    3.Import the processed file into Bitwarden
     """
-    if not output_file:
-        output_file = (Path(input_file).parent / OUTPUT_FILE_DEFAULT).as_posix()
-
-    if os.path.exists(output_file) and not force:
-        click.echo(f"Output file {output_file} already exists. Use --force to overwrite.")
+    output_path = (
+        Path(output_file)
+        if output_file
+        else Path(input_file).parent / f"{OUTPUT_FILE_DEFAULT}.csv"  # f".{format}"
+    )
+    if output_path.exists() and not force:
+        click.echo(f"Output file {output_path} already exists. Use --force to overwrite.")
         raise click.Abort()
 
-    with (
-        open(input_file, newline="", encoding="utf-8") as infile,
-        open(output_file, "w", newline="", encoding="utf-8") as outfile,
-    ):
+    if output_format == "csv":
+        writer = BitwardenCsv(output_path)
+    else:
+        writer = BitwardenCsv(output_path)  # todo: implement json export
+
+    with open(input_file, newline="", encoding="utf-8") as infile:
         reader = csv.reader(infile, delimiter=",")
-        writer = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
-
-        # Bitwarden CSV header: https://bitwarden.com/help/condition-bitwarden-import/
-        header = [
-            "folder",
-            "favorite",
-            "type",
-            "name",
-            "notes",
-            "fields",
-            "reprompt",
-            "login_uri",
-            "login_username",
-            "login_password",
-            "login_totp",
-        ]
-        writer.writerow(header)
-
         for row in reader:
             if row and not row[0].startswith("mSecure"):
-                writer.writerow(convert_row(row, extra_fields == NOTES_MODE))
+                data = import_msecure_row(row, extra_fields == NOTES_MODE)
+                writer.write_record(data)
 
-    click.echo(f"Bitwarden CSV saved to {output_file}")
-
-
-def convert_row(row: List[str], extra_fields_to_notes: bool) -> List[str]:
-    """Convert mSecure row to Bitwarden row."""
-    name = row[0].split("|")[0]
-    if len(row[0].split("|")) > 2:
-        print(f"Warning: name has more than one '|' character :`{row[0]}`.")
-    record_type = "login"
-    if row[1].strip() not in ["Login", "Credit Card", "Email Account"]:
-        print(f"Warning: record type is not 'Login' :`{row[1]}`.")
-    tag = row[2].strip()
-    notes = row[3].replace("\\n", "\n")
-    field_values = {
-        "Website": "",
-        "Username": "",
-        "Password": "",
-        "Card Number": "",
-        "Security Code": "",
-        "PIN": "",
-        # "Name on Card": "",
-        # "Expiration Date": "",
-    }
-    fields = {}
-    for field in row[4:]:
-        parts = field.split("|")
-        if parts[0] in field_values:
-            if field_values[parts[0]]:
-                print(f"Warning: Duplicate field `{parts[0]}` in row `{row}`.")
-            field_values[parts[0]] = "|".join(parts[2:])
-        elif any(value.strip() for value in parts[2:]):
-            if extra_fields_to_notes:
-                notes += f"\n{parts[0]}: {','.join(parts[2:])}"
-            else:
-                fields[parts[0]] = ",".join(parts[2:])
-    password, username = get_creds(field_values, row)
-    if field_values["Card Number"]:
-        if tag:
-            click.echo(f"Warning: Tag `{tag}` present for Card, override with `card`:\n{row}")
-        tag = "card"
-    if not username and not password and not field_values["Website"]:
-        record_type = "note"
-    if field_values["PIN"]:
-        fields["PIN"] = field_values["PIN"]
-
-    return [
-        tag,  # folder
-        "",  # favorite
-        record_type,  # type
-        name,  # name
-        notes,  # notes
-        "\n".join([f"{name}: {value}" for name, value in fields.items()]),  # fields
-        "",  # reprompt
-        field_values["Website"],  # login_uri
-        username,  # login_username
-        password,  # login_password
-        "",  # login_totp
-    ]
-
-
-def get_creds(field_values: Dict[str, str], row: List[str]) -> Tuple[str, str]:
-    """Get username and password."""
-    username = field_values["Card Number"] or field_values["Username"]
-    password = field_values["Security Code"] or field_values["Password"]
-    if field_values["Card Number"] and field_values["Username"]:
-        click.echo(f"Error: Both Card Number and Username present in row:\n{row}")
-    if field_values["Security Code"] and field_values["Password"]:
-        click.echo(f"Error: Both Security Code and Password present in row:\n{row}")
-    return password, username
+    writer.close()
+    click.echo(f"File to import into Bitwarden saved to {output_path}")
 
 
 if __name__ == "__main__":  # pragma: no cover
